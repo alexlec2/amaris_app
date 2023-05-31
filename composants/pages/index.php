3fb2745d99c13a105741eb021369641c32caa1ba
Alexandre Leclerc <leclercalex3@gmail.com>
2023-05-31 12:07:00 +0200

    font link 2

--- composants/pages/index.php
+++ composants/pages/index.php
@@ -10,9 +10,7 @@
     <script src="./../../assets/js/fonctions.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mukta+Mahee:wght@500&display=swap" rel="stylesheet">
+    
     <script src="../../scroll-entrance-master/src/scroll-entrance.js"></script>
 </head>
 <body>
